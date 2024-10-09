e40b25b4176628dcdbd37a2ea6a74d7310291ee0
Yamazaki-hnut <yamazaki.hnut@gmail.com>
2024-10-09 12:30:42 +0900

    cloudfirestore追加

--- lib/pages/question_page.dart
+++ lib/pages/question_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+
 class QuestionPage extends StatefulWidget {
   const QuestionPage({super.key});
 

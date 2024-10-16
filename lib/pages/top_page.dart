93298d0da0130a7650863add371cafc413b00ff3
Fukamachi-hnut <fukamachi.hnut@gmail.com>
2024-10-16 11:45:08 +0900

    remote config追加。db_versionの値が変わったときのみfirebaseから取得

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:fe_project/pages/settings_page.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -26,6 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isFirstLaunch = true; // 初回起動のフラグ
   List<Map<String, dynamic>> maps = [];
   double progress = 0; // 進捗率
+  final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
 
   @override
   void initState() {
@@ -35,10 +37,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _initDbAndFetchData() async {
     _database = await initializeDb(); // ローカルデータベースの初期化
+    await remoteConfig.setConfigSettings(RemoteConfigSettings(
+      fetchTimeout: const Duration(seconds: 10),
+      minimumFetchInterval: Duration.zero,
+    ));
 
-    // 初回起動時のみFirestoreからデータを取得
-    if (_isFirstLaunch) {
-      await loadQuizData(); // ローカルDBにデータを読み込み
+    await remoteConfig.setDefaults(<String, dynamic>{
+      "db_version": 0,
+    });
+
+    await remoteConfig.fetchAndActivate();
+    int remoteDbVersion = remoteConfig.getInt("db_version");
+    print("remote DBバージョン: $remoteDbVersion");
+
+    // ローカルdbからバージョンを取得
+    final appConfig = await _database!.query('appConfig');
+    Object? localDbVersion = appConfig[0]['db_version'];
+    print("ローカルDBバージョン: $localDbVersion");
+
+    // remote configのバージョンと違ったらfirebaseから取得
+    if (_isFirstLaunch && remoteDbVersion != localDbVersion) {
+      await loadQuizData(remoteDbVersion); // ローカルDBにデータを読み込み
       _isFirstLaunch = false; // フラグを更新
     }
 
@@ -56,78 +75,52 @@ class _MyHomePageState extends State<MyHomePage> {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, 'quiz_data.db'); // DBのパスを指定
 
-    // 既存のデータベースがあれば削除しない
-    final fileExists = await databaseExists(path);
-
-    // 既存のデータベースがあれば削除、作成
-    // if (fileExists) {
-    //   print('Deleting existing database...');
-    //   await deleteDatabase(path); // データベースを削除
-    //   openDatabase(
-    //     path,
-    //     onCreate: (db, version) {
-    //       // テーブルの作成
-    //       return db.execute(
-    //         'CREATE TABLE quizData('
-    //             'id INTEGER PRIMARY KEY, '
-    //             'answer TEXT, '
-    //             'comment TEXT, '
-    //             'image TEXT, '
-    //             'link TEXT, '
-    //             'mistake1 TEXT, '
-    //             'mistake2 TEXT, '
-    //             'mistake3 TEXT, '
-    //             'question TEXT, '
-    //             'quiz_id INTEGER, '
-    //             'series_document_id TEXT, '
-    //             'series_name TEXT, '
-    //             'stage_document_id TEXT, '
-    //             'stage_name TEXT, '
-    //             'judge INTEGER'
-    //             ')',
-    //       );
-    //     },
-    //     version: 1,
-    //   );
+    // db削除
+    // if (_isFirstLaunch) {
+    // await deleteDatabase(path);
     // }
 
-
-    if (fileExists) {
-      print('Using existing database...'); // 既存のデータベースを使用
-    } else {
-      print('Creating a new database...');
-      return openDatabase(
-        path,
-        onCreate: (db, version) {
-          // テーブルの作成
-          return db.execute(
-            'CREATE TABLE quizData('
-                'id INTEGER PRIMARY KEY, '
-                'answer TEXT, '
-                'comment TEXT, '
-                'image TEXT, '
-                'link TEXT, '
-                'mistake1 TEXT, '
-                'mistake2 TEXT, '
-                'mistake3 TEXT, '
-                'question TEXT, '
-                'quiz_id INTEGER, '
-                'series_document_id TEXT, '
-                'series_name TEXT, '
-                'stage_document_id TEXT, '
-                'stage_name TEXT, '
-                'judge INTEGER'
-                ')',
-          );
-        },
-        version: 1,
-      );
-    }
-
-    return openDatabase(path, version: 1);
+    return openDatabase(
+      path,
+      onCreate: (db, version) async {
+        // quizDataテーブルの作成
+        await db.execute(
+          'CREATE TABLE quizData('
+          'id INTEGER PRIMARY KEY, '
+          'answer TEXT, '
+          'comment TEXT, '
+          'image TEXT, '
+          'link TEXT, '
+          'mistake1 TEXT, '
+          'mistake2 TEXT, '
+          'mistake3 TEXT, '
+          'question TEXT, '
+          'quiz_id INTEGER, '
+          'series_document_id TEXT, '
+          'series_name TEXT, '
+          'stage_document_id TEXT, '
+          'stage_name TEXT, '
+          'judge INTEGER'
+          ')',
+        );
+        // appConfigテーブルの作成
+        await db.execute(
+          'CREATE TABLE appConfig('
+          'id INTEGER PRIMARY KEY, '
+          'db_version INTEGER'
+          ')',
+        );
+        await db.insert('appConfig', {'id': 1, 'db_version': 0});
+      },
+      version: 1,
+    );
   }
 
-  Future<void> loadQuizData() async {
+  Future<void> loadQuizData(int remoteDbVersion) async {
+    print("データをロードします");
+    // ローカルDBのバージョンを設定
+    await _database!.update('appConfig', {'db_version': remoteDbVersion});
+
     // Firestoreからデータを取得
     final snapshot = await FirebaseFirestore.instance
         .collection('contents')
@@ -137,12 +130,14 @@ class _MyHomePageState extends State<MyHomePage> {
         .get();
 
     // Firestoreから取得したデータのリストを保持
-    List<Map<String, dynamic>> firestoreDataList = List.from(snapshot.data()!['quizDataList']);
+    List<Map<String, dynamic>> firestoreDataList =
+        List.from(snapshot.data()!['quizDataList']);
 
     // データをSQLiteに保存
     for (var doc in firestoreDataList) {
       // すでにデータが存在するかを確認
-      final existingData = await _database!.query('quizData', where: 'id = ?', whereArgs: [doc['quiz_id']]);
+      final existingData = await _database!
+          .query('quizData', where: 'id = ?', whereArgs: [doc['quiz_id']]);
 
       if (existingData.isEmpty) {
         // 存在しない場合は新規挿入
@@ -164,7 +159,6 @@ class _MyHomePageState extends State<MyHomePage> {
           'judge': 0, // 初期値
         });
       }
-
     }
 
     // DBからクイズデータを取得
@@ -180,7 +174,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // 進捗を計算（judgeが2の問題数）
     setState(() {
       correctAnswers = quizDataList.where((quiz) => quiz['judge'] == 2).length;
-      progress = totalQuestions > 0 ? correctAnswers / totalQuestions : 0; // 進捗を計算
+      progress =
+          totalQuestions > 0 ? correctAnswers / totalQuestions : 0; // 進捗を計算
     });
     print(correctAnswers);
     print(progress);
@@ -195,7 +190,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // 画面が小さい場合の最低限のサイズを設定
     final double adjustedScreenHeight = screenHeight < 600 ? 600 : screenHeight;
     final double adjustedScreenWidth = screenWidth < 300 ? 300 : screenWidth;
-
 
     return Scaffold(
       appBar: AppBar(
@@ -215,94 +209,101 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
       ),
-
       body: SafeArea(
         child: Container(
           color: const Color(0xFFE4F9F5), // SafeAreaの背景色を設定
           child: isLoading
               ? Center(
-            child: CircularProgressIndicator(), // ローディングインジケーターを表示
-          )
+                  child: CircularProgressIndicator(), // ローディングインジケーターを表示
+                )
               : Column(
-            mainAxisAlignment: MainAxisAlignment.center, // 縦方向に中央揃え
-            children: [
-              // 正解数と進捗バーを表示
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: screenHeight * 0.05),
-                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center, // 縦方向に中央揃え
                   children: [
-                    Text(
-                      '正解数 $correctAnswers / $totalQuestions 問中',
-                      style: TextStyle(
-                        fontSize: screenHeight * 0.03, // フォントサイズを画面高さに基づいて指定
-                        color: Colors.black,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: screenHeight * 0.02),
-                    SizedBox(
-                      width: screenWidth * 0.8, // バーの幅
-                      height: screenHeight * 0.03, // バーの高さ
-                      child: Stack(
+                    // 正解数と進捗バーを表示
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: screenHeight * 0.05),
+                      child: Column(
                         children: [
-                          Container(
-                            width: screenWidth * 0.8,
-                            height: screenHeight * 0.03,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 0),
-                              borderRadius: BorderRadius.circular(5),
+                          Text(
+                            '正解数 $correctAnswers / $totalQuestions 問中',
+                            style: TextStyle(
+                              fontSize:
+                                  screenHeight * 0.03, // フォントサイズを画面高さに基づいて指定
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: screenHeight * 0.02),
+                          SizedBox(
+                            width: screenWidth * 0.8, // バーの幅
+                            height: screenHeight * 0.03, // バーの高さ
+                            child: Stack(
+                              children: [
+                                Container(
+                                  width: screenWidth * 0.8,
+                                  height: screenHeight * 0.03,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black, width: 0),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                ),
+                                // アニメーション付きのバー
+                                AnimatedContainer(
+                                  duration: const Duration(milliseconds: 500),
+                                  // アニメーションの時間
+                                  width: screenWidth * 0.8 * progress,
+                                  // 進捗率に基づいてバーの幅を調整
+                                  height: screenHeight * 0.03,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(width: 0),
+                                    borderRadius: BorderRadius.circular(5),
+                                    color: const Color(0xFF30E3CA),
+                                  ),
+                                ),
+                                // パーセンテージをバーの中に表示
+                                Center(
+                                  child: Text(
+                                    '${(progress * 100).toInt()}%', // パーセンテージを表示
+                                    style: TextStyle(
+                                      color: Colors.black, // テキストカラー
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          // アニメーション付きのバー
-                          AnimatedContainer(
-                            duration: const Duration(milliseconds: 500), // アニメーションの時間
-                            width: screenWidth * 0.8 * progress, // 進捗率に基づいてバーの幅を調整
-                            height: screenHeight * 0.03,
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 0),
-                              borderRadius: BorderRadius.circular(5),
-                              color: const Color(0xFF30E3CA),
-                            ),
-                          ),
-                          // パーセンテージをバーの中に表示
-                          Center(
-                            child: Text(
-                              '${(progress * 100).toInt()}%', // パーセンテージを表示
-                              style: TextStyle(
-                                color: Colors.black, // テキストカラー
-                              ),
-                            ),
-                          ),
+                          SizedBox(height: screenHeight * 0.02),
+// 50%と100%のラベルは削除、必要に応じて保持可能
                         ],
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.02),
-// 50%と100%のラベルは削除、必要に応じて保持可能
-
+                    // カテゴリのリスト
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildCategoryTile(screenWidth, screenHeight,
+                              "テクノロジー", 0, context, "technologyStage"),
+                          _buildCategoryTile(screenWidth, screenHeight,
+                              "マネジメント", 1, context, "managementStage"),
+                          _buildCategoryTile(screenWidth, screenHeight, "ストラテジ",
+                              2, context, "strategyStage"),
+                          _buildCategoryTile(screenWidth, screenHeight,
+                              "全範囲から出題", 3, context, "allStage"),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-              ),
-              // カテゴリのリスト
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _buildCategoryTile(screenWidth, screenHeight, "テクノロジー", 0, context, "technologyStage"),
-                    _buildCategoryTile(screenWidth, screenHeight, "マネジメント", 1, context, "managementStage"),
-                    _buildCategoryTile(screenWidth, screenHeight, "ストラテジ", 2, context, "strategyStage"),
-                    _buildCategoryTile(screenWidth, screenHeight, "全範囲から出題", 3, context, "allStage"),
-                  ],
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
   }
 
-
-  Widget _buildCategoryTile(double screenWidth, double screenHeight, String category, int index, BuildContext context, String categoryName) {
+  Widget _buildCategoryTile(double screenWidth, double screenHeight,
+      String category, int index, BuildContext context, String categoryName) {
     return GestureDetector(
       onTapDown: (_) {
         // タップしたらスワイプのアニメーションを開始
@@ -321,7 +322,8 @@ class _MyHomePageState extends State<MyHomePage> {
           await Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) =>  QuestionPage(categoryName), // QuestionPageに遷移
+              builder: (context) =>
+                  QuestionPage(categoryName), // QuestionPageに遷移
             ),
           );
         } else {
@@ -341,11 +343,13 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       },
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.1, vertical: screenHeight * 0.02), // マージンを設定
-        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.03), // パディングを設定
+        margin: EdgeInsets.symmetric(
+            horizontal: screenWidth * 0.1, vertical: screenHeight * 0.02),
+        // マージンを設定
+        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.03),
+        // パディングを設定
         decoration: BoxDecoration(
-          border:
-          Border.all(
+          border: Border.all(
             color: Color(0xFF11999E), // 枠線の色を設定
             width: 2,
           ),

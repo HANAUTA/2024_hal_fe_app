1dda26aab111b4501bd52aceab557a68fb243bfd
Yamazaki-hnut <yamazaki.hnut@gmail.com>
2024-10-24 12:57:19 +0900

    toppageにアニメーション追加

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -7,6 +7,7 @@ import 'package:path/path.dart';
 import 'package:cloud_firestore/cloud_firestore.dart'; // Firestoreをインポート
 import 'package:fe_project/pages/category_page.dart';
 import 'question_page.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -50,7 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
     setProgress();
   }
 
-
   Future<void> setProgress() async {
     Map<String, dynamic> progressData = await quizDataInstance.getProgress(quizDataList: quizDataList);
     // 進捗を計算（judgeが2の問題数）
@@ -62,7 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
       totalAnswersCount = progressData['totalAnswersCount'];
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -84,9 +83,9 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(
             icon: Icon(Icons.settings),
 
-            onPressed: () async{
+            onPressed: () async {
               // 設定ページへの遷移
-             await Navigator.push(
+              await Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => SettingsPage(), // 設定ページに遷移
@@ -102,123 +101,119 @@ class _MyHomePageState extends State<MyHomePage> {
           color: const Color(0xFFFAF8F1), // SafeAreaの背景色を設定
           child: isLoading
               ? Center(
-                  child: CircularProgressIndicator(), // ローディングインジケーターを表示
-                )
+            child: CircularProgressIndicator(), // ローディングインジケーターを表示
+          )
               : Column(
-                  mainAxisAlignment: MainAxisAlignment.center, // 縦方向に中央揃え
+            mainAxisAlignment: MainAxisAlignment.center, // 縦方向に中央揃え
+            children: [
+              // 正解数と進捗バーを表示
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: screenHeight * 0.05),
+                child: Column(
                   children: [
-                    // 正解数と進捗バーを表示
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: screenHeight * 0.05),
-                      child: Column(
+                    Text(
+                      '学習数 $totalAnswersCount / $totalQuestionsCount 問中',
+                      style: TextStyle(
+                        fontSize: screenHeight * 0.03, // フォントサイズを画面高さに基づいて指定
+                        color: Colors.black87, // 色を少しソフトに
+                        fontWeight: FontWeight.bold, // フォントを少し太く
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: screenHeight * 0.01), // 少しスペースを追加
+                    Text(
+                      "正解: ${(correctProgress * 100).toInt()}%  不正解: ${(wrongProgress * 100).toInt()}%",
+                      style: TextStyle(
+                        fontSize: screenHeight * 0.025,
+                        color: Colors.black54, // テキストの色を少し薄く
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: screenHeight * 0.03), // 高さを調整
+                    SizedBox(
+                      width: screenWidth * 0.85, // バーの幅を少し広げる
+                      height: screenHeight * 0.035, // バーの高さを少し高く
+                      child: Stack(
                         children: [
-                          Text(
-                            '学習数 $totalAnswersCount / $totalQuestionsCount 問中',
-                            style: TextStyle(
-                              fontSize: screenHeight * 0.03, // フォントサイズを画面高さに基づいて指定
-                              color: Colors.black87, // 色を少しソフトに
-                              fontWeight: FontWeight.bold, // フォントを少し太く
+                          Container(
+                            width: screenWidth * 0.85,
+                            height: screenHeight * 0.035,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8), // 角を丸める
+                              border: Border.all(
+                                color: Colors.grey.shade300, // 薄いグレーの枠線
+                                width: 1,
+                              ),
                             ),
-                            textAlign: TextAlign.center,
                           ),
-                          SizedBox(height: screenHeight * 0.01), // 少しスペースを追加
-                          Text(
-                            "正解: ${(correctProgress * 100).toInt()}%  不正解: ${(wrongProgress * 100).toInt()}%",
-                            style: TextStyle(
-                              fontSize: screenHeight * 0.025,
-                              color: Colors.black54, // テキストの色を少し薄く
+                          // 背景の白いバー
+                          Container(
+                            width: screenWidth * 0.85,
+                            height: screenHeight * 0.035,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: const Color(0xFFF0F0F0), // 薄いグレーの背景
                             ),
-                            textAlign: TextAlign.center,
                           ),
-                          SizedBox(height: screenHeight * 0.03), // 高さを調整
-                          SizedBox(
-                            width: screenWidth * 0.85, // バーの幅を少し広げる
-                            height: screenHeight * 0.035, // バーの高さを少し高く
-                            child: Stack(
-                              children: [
-                                Container(
-                                  width: screenWidth * 0.85,
-                                  height: screenHeight * 0.035,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8), // 角を丸める
-                                    border: Border.all(
-                                      color: Colors.grey.shade300, // 薄いグレーの枠線
-                                      width: 1,
-                                    ),
-                                  ),
-                                ),
-                                // 背景の白いバー
-                                Container(
-                                  width: screenWidth * 0.85,
-                                  height: screenHeight * 0.035,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    color: const Color(0xFFF0F0F0), // 薄いグレーの背景
-                                  ),
-                                ),
-                                // 不正解の赤いバー
-                                AnimatedContainer(
-                                  duration: const Duration(milliseconds: 500),
-                                  width: screenWidth * 0.85 * (correctProgress + wrongProgress),
-                                  height: screenHeight * 0.035,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    color: const Color(0xFFFF6969), // 赤いバーの色
-                                  ),
-                                ),
-                                // 正解の緑色のバー
-                                AnimatedContainer(
-                                  duration: const Duration(milliseconds: 500),
-                                  width: screenWidth * 0.85 * correctProgress,
-                                  height: screenHeight * 0.035,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black26, // バーに影を追加
-                                        offset: Offset(0, 2),
-                                        blurRadius: 4,
-                                      ),
-                                    ],
-                                    color: const Color(0xFF11999E), // 緑のバーの色
-                                  ),
+                          // 不正解の赤いバー
+                          AnimatedContainer(
+                            duration: const Duration(milliseconds: 500),
+                            width: screenWidth * 0.85 * (correctProgress + wrongProgress),
+                            height: screenHeight * 0.035,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: const Color(0xFFFF6969), // 赤いバーの色
+                            ),
+                          ),
+                          // 正解の緑色のバー
+                          AnimatedContainer(
+                            duration: const Duration(milliseconds: 500),
+                            width: screenWidth * 0.85 * correctProgress,
+                            height: screenHeight * 0.035,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26, // バーに影を追加
+                                  offset: Offset(0, 2),
+                                  blurRadius: 4,
                                 ),
                               ],
+                              color: const Color(0xFF11999E), // 緑のバーの色
                             ),
                           ),
-                          SizedBox(height: screenHeight * 0.03),
                         ],
                       ),
                     ),
-
-                    // カテゴリのリスト
-                    Expanded(
-                      child: Scrollbar(
-                        thickness: 8,
-                        thumbVisibility: true,
-                        child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              _buildCategoryTile(
-                                  screenWidth, screenHeight, "テクノロジー系", 0, context, "technologyStage"),
-                              _buildCategoryTile(
-                                  screenWidth, screenHeight, "マネジメント系", 1, context, "managementStage"),
-                              _buildCategoryTile(
-                                  screenWidth, screenHeight, "ストラテジ系", 2, context, "strategyStage"),
-                              _buildCategoryTile(
-                                  screenWidth, screenHeight, "全範囲から出題", 3, context, "allStage"),
-                              // if (wrongAnswersCount > 0)
-                              //   _buildCategoryTile(
-                              //       screenWidth, screenHeight, "間違えた問題から出題 ($wrongAnswersCount問)", 3, context, "wrongStage"),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-
+                    SizedBox(height: screenHeight * 0.03),
                   ],
                 ),
+              ),
+
+              // カテゴリのリスト
+              Expanded(
+                child: Scrollbar(
+                  thickness: 8,
+                  thumbVisibility: true,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildCategoryTile(
+                            screenWidth, screenHeight, "テクノロジー系", 0, context, "technologyStage"),
+                        _buildCategoryTile(
+                            screenWidth, screenHeight, "マネジメント系", 1, context, "managementStage"),
+                        _buildCategoryTile(
+                            screenWidth, screenHeight, "ストラテジ系", 2, context, "strategyStage"),
+                        _buildCategoryTile(
+                            screenWidth, screenHeight, "全範囲から出題", 3, context, "allStage"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -226,61 +221,65 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget _buildCategoryTile(double screenWidth, double screenHeight,
       String displayCategoryName, int index, BuildContext context, String categoryName) {
-    return GestureDetector(
-      onTapDown: (_) {
-        // タップしたらスワイプのアニメーションを開始
-        setState(() {
-          _isTappedList[index] = true;
-        });
-      },
-      onTapUp: (_) async {
-        // タップが離れたらスワイプを解除
-        setState(() {
-          _isTappedList[index] = false;
-        });
+    return AnimationConfiguration.staggeredList(
+      position: index,
+      duration: const Duration(milliseconds: 375),
+      child: SlideAnimation(
+        verticalOffset: 50.0,
+        child: FadeInAnimation(
+          child: GestureDetector(
+            onTapDown: (_) {
+              setState(() {
+                _isTappedList[index] = true;
+              });
+            },
+            onTapUp: (_) async {
+              setState(() {
+                _isTappedList[index] = false;
+              });
 
-        // "全範囲から出題"が押された場合のみQuestionPageに遷移
-        if (categoryName == "allStage" || categoryName == "wrongStage") {
-          await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  QuestionPage(categoryName), // QuestionPageに遷移
-            ),
-          );
-        } else {
-          await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CategoryPage(categoryName), // カテゴリページに遷移
-            ),
-          );
-        }
-        _initQuizData();
-      },
-      onTapCancel: () {
-        // タップがキャンセルされたらスワイプを解除
-        setState(() {
-          _isTappedList[index] = false;
-        });
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.1, vertical: screenHeight * 0.02),
-        // マージンを設定
-        padding: EdgeInsets.symmetric(vertical: screenHeight * 0.03),
-        // パディングを設定
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Color(0xFFC58940), // 枠線の色を設定
-            width: 2,
-          ),
-        ),
-        child: Center(
-          child: Text(
-            displayCategoryName,
-            style: TextStyle(
-              fontSize: screenHeight * 0.025, // フォントサイズを画面高さに基づいて指定
+              // "全範囲から出題"が押された場合のみQuestionPageに遷移
+              if (categoryName == "allStage" || categoryName == "wrongStage") {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => QuestionPage(categoryName), // QuestionPageに遷移
+                  ),
+                );
+              } else {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CategoryPage(categoryName), // カテゴリページに遷移
+                  ),
+                );
+              }
+              _initQuizData();
+            },
+            onTapCancel: () {
+              // タップがキャンセルされたらスワイプを解除
+              setState(() {
+                _isTappedList[index] = false;
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.1, vertical: screenHeight * 0.02),
+              padding: EdgeInsets.symmetric(vertical: screenHeight * 0.03),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color(0xFFC58940), // 枠線の色を設定
+                  width: 2,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  displayCategoryName,
+                  style: TextStyle(
+                    fontSize: screenHeight * 0.025, // フォントサイズを画面高さに基づいて指定
+                  ),
+                ),
+              ),
             ),
           ),
         ),
